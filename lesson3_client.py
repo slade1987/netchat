09ec61fe119345b3df591139d48fcd0c9347e4af
slade <paran87@mail.ru>
2023-05-01 19:50:36 +0300

    Lesson 3 add JIM

--- lesson3_client.py
+++ lesson3_client.py
@@ -13,18 +13,19 @@ PRESENCE = {
 "time": str(datetime.now())
 }
 
-def resp_ans(ans):
-    pass
+def parse_data(data:dict):
+    if data['response'][0] == '200':
+        return print("Все прошло хорошо")
+    else:
+        return print('Что то пошло не так')
+
 
 def chat_client(ip:str, port: int):
     sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
     sock.connect((ip, int(port)))
     data = json.loads(sock.recv(1024))
     sock.send(json.dumps(PRESENCE).encode('utf-8'))
-    if data['response'][0] == '200':
-        print("Все прошло хорошо")
-    else:
-        print('Что то пошло не так')
+    parse_data(data)
     sock.close()
     print(data)
 
---
+++ test.py
@@ -0,0 +1,28 @@
+# import socket
+# import unittest
+# from lesson3_client import chat_client
+# from lesson3_server import chat_server
+#
+# class TestChat(unittest.TestCase):
+#     def setUp(self):
+#         self.serv = chat_server('localhost', 7777)
+#         self.client = chat_client('localhost', 7777)
+#
+#     def connClose(self):
+#         self.serv.close()
+#         self.client.close()
+#
+#     def test_serv_sock_is_inst(self):
+#         self.assertIsInstance(self.serv,socket.socket)
+#
+#     def testServer(self):
+#         self.assertEqual(self.serv.getsockname(), ('127.0.0.1', 7777))
+#
+#     # def testClient(self):
+#     #     self.assertEqual(self.client.getsockname(),('127.0.0.1',7777))
+#
+#
+#
+#
+# if __name__ == '__main__':
+#     unittest.main()

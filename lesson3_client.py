884196bbcc1c59cf2707dd06d52553e92b74b0fc
slade <paran87@mail.ru>
2023-05-01 03:33:53 +0300

    Lesson 3 add argparse

--- lesson3_client.py
+++ lesson3_client.py
@@ -1,16 +1,27 @@
 import socket
 import argparse
+import json
+from datetime import datetime
 
 parser = argparse.ArgumentParser()
 parser.add_argument('ip_addr',  nargs='?', default='localhost')
 parser.add_argument("port", nargs='?', default='7777')
 args = parser.parse_args()
 
+RESPONSE = {
+"response": 'None',
+"From": 'client',
+"time": str(datetime.now())
+}
+
+def resp_ans(ans):
+    pass
+
 def chat_client(ip:str, port: int):
     sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
     sock.connect((ip, int(port)))
-    sock.send(bytes("hello", encoding = 'UTF-8'))
-    data = sock.recv(1024)
+    data = json.loads(sock.recv(1024))
+    sock.send(json.dumps(RESPONSE).encode('utf-8'))
     sock.close()
     print(data)
 
--- lesson3_server.py
+++ lesson3_server.py
@@ -1,10 +1,20 @@
 import socket
 import argparse
+from datetime import datetime
+import json
 
 parser = argparse.ArgumentParser()
 parser.add_argument('ip_addr',  nargs='?', default='localhost')
 parser.add_argument("port", nargs='?', default='7777')
 args = parser.parse_args()
+
+RESPONSE = {
+"response": 'None',
+"From": 'Server',
+"time": str(datetime.now())
+}
+
+
 
 def chat_server(ip:str, port:int):
     sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
@@ -15,10 +25,10 @@ def chat_server(ip:str, port:int):
     while True:
         conn, addr = sock.accept()
         print('Conn', conn, '\n', 'Add', addr  )
-        data = conn.recv(1024)
-        print(str(data))
-        conn.send(data.upper())
+        conn.send(json.dumps(RESPONSE).encode('utf-8'))
+        data = json.loads(conn.recv(1024))
         conn.close()
+        print(data)
 
 if __name__ == '__main__':
     chat_server(args.ip_addr, args.port)

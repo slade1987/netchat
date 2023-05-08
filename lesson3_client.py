166277904ee9dc42ce934c9a223c9da048954569
slade <paran87@mail.ru>
2023-05-08 15:50:55 +0300

    Lesson 5 client_log

--- lesson3_client.py
+++ lesson3_client.py
@@ -2,6 +2,7 @@ import socket
 import argparse
 import json
 from datetime import datetime
+import log.client_log_config as logg
 
 parser = argparse.ArgumentParser()
 parser.add_argument('ip_addr',  nargs='?', default='localhost')
@@ -22,13 +23,23 @@ def parse_data(data:dict):
 
 def chat_client(ip:str, port: int):
     sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-    sock.connect((ip, int(port)))
-    data = json.loads(sock.recv(1024))
-    sock.send(json.dumps(PRESENCE).encode('utf-8'))
-    # sock.send("dfdfdfssdf".encode('utf-8'))
-    parse_data(data)
-    sock.close()
-    print(data)
+    try:
+       if (sock.connect((ip,int(port)))):
+           logg.logger.info("Соеденение прошло успешно")
+           try:
+                data = json.loads(sock.recv(1024))
+                sock.send(json.dumps(PRESENCE).encode('utf-8'))
+                logg.logger.info()
+                parse_data(data)
+                sock.close()
+                print(data)
+                logg.logger.info("Данные получены и отправленые")
+           except:
+               logg.logger.warning("Данные не удалось отправеть или получить")
+
+    except:
+        logg.logger.error("Не удалось установить соединение")
+
 
 if __name__ == "__main__":
     chat_client(args.ip_addr, args.port)

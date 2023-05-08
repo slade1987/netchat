e058938bec2f3ecb0f2e84f4976b7e5c3aba19a3
slade <paran87@mail.ru>
2023-05-08 17:24:58 +0300

    Lesson 5 client_log

--- lesson3_client.py
+++ lesson3_client.py
@@ -3,6 +3,7 @@ import argparse
 import json
 from datetime import datetime
 import log.client_log_config as logg
+from functools import wraps
 
 parser = argparse.ArgumentParser()
 parser.add_argument('ip_addr',  nargs='?', default='localhost')
@@ -14,29 +15,32 @@ PRESENCE = {
 "time": str(datetime.now())
 }
 
+def log(func):
+    @wraps(func)
+    def call(*args, **kwargs):
+        print(func.__name__)
+        return func(*args, **kwargs)
+    return call
+
+@log
 def parse_data(data:dict):
     if data['response'][0] == '200':
         return print("Все прошло хорошо")
     else:
         return print('Что то пошло не так')
 
-
+@log
 def chat_client(ip:str, port: int):
     sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
     try:
-       if (sock.connect((ip,int(port)))):
-           logg.logger.info("Соеденение прошло успешно")
-           try:
-                data = json.loads(sock.recv(1024))
-                sock.send(json.dumps(PRESENCE).encode('utf-8'))
-                logg.logger.info()
-                parse_data(data)
-                sock.close()
-                print(data)
-                logg.logger.info("Данные получены и отправленые")
-           except:
-               logg.logger.warning("Данные не удалось отправеть или получить")
-
+       sock.connect((ip,int(port)))
+       logg.logger.info("Соеденение прошло успешно")
+       data = json.loads(sock.recv(1024))
+       sock.send(json.dumps(PRESENCE).encode('utf-8'))
+       parse_data(data)
+       sock.close()
+       print(data)
+       logg.logger.info("Данные получены и отправленые")
     except:
         logg.logger.error("Не удалось установить соединение")
 
